92ca7056c62029133c52fb5a2c5ea7f100d6b10e
Mylyn-1122 <69164770+Mylyn-1122@users.noreply.github.com>
2025-01-11 23:40:06 -0800

    Redid sign up

--- login.js
+++ login.js
@@ -1,69 +1,47 @@
-import { 
-    getAuth, createUserWithEmailAndPassword, 
-    onAuthStateChanged, signInWithEmailAndPassword, signOut
-} from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
 
-const auth = getAuth();
-
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const signUpBtn = document.getElementById("signup-btn");
-const signInBtn = document.getElementById("login-btn");
-const signOutBtn = document.getElementById("logout-btn");
-
-
-
-onAuthStateChanged(auth, (user) => {
-    //console.log(user);
-    if(user){
-        window.location.href = "second.html";
-    }else{
-        window.location.href = "login.html";
-    }
-
-});
-
-const signUpButtonPressed = async (e) => {
-    //console.log("Pressed!");
-    e.preventDefault();
-
-    try{
-
-        const userCredential  = await createUserWithEmailAndPassword(auth, email.value, password.value);
-       // console.log(userCredential);
-       
-    }catch(error){ 
-       console.log(error.code);
-       
-    }
+const firebaseConfig = {
+    apiKey: "AIzaSyB79YeHcsM6QGu5v4IR5fm4cJtGcmqzB5w",
+    authDomain: "login-with-firebase-20bdf.firebaseapp.com",
+    projectId: "login-with-firebase-20bdf",
+    storageBucket: "login-with-firebase-20bdf.firebasestorage.app",
+    messagingSenderId: "449252389640",
+    appId: "1:449252389640:web:1b076ec92f1ba15056b2bd",
+    measurementId: "G-2M2KX00L6Z"
 };
 
-const signOutButtonPressed = async () => {
-    //console.log("Pressed!");
-    try{
-        await signOut(auth);
-        email.value = "";
-        password.value = "";
-        window.location.href = "login.html";
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-    }catch(error){
-        console.log(error);
-    }
+const email = document.getElementById("email").value;
+const password = document.getElementById("password").value;
+const loginBT = document.getElementById("loginBTN")
+const signUpBT = document.getElementById("signUpBTN")
 
-};
+// function loginFunc (e) {
+//     e.preventDefault()
+// }
 
-const signInButtonPressed = async (e) => {
-    //console.log("Pressed!");
-    e.preventDefault();
-    try{
-        await signInWithEmailAndPassword(auth, email.value, password.value);
 
-    }catch(error){
-        console.log(error);
-    }
+signUpBT.addEventListener("click", function (event) {
+    event.preventDefault()
 
-};
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
-signUpBtn.addEventListener("click", signUpButtonPressed);
-signInBtn.addEventListener("click", signInButtonPressed);
-signOutBtn.addEventListener("click", signOutButtonPressed);
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed up 
+            const user = userCredential.user;
+            // ...
+            alert("signed up")
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert(errorMessage)
+            // ..
+        });
+
+})
