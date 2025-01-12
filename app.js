ba87a3bc4455044a8923168e3486ee44f5dc49fe
Siyou-Wang <SiyouWang1@gmail.com>
2025-01-11 22:08:44 -0800

    logout button glitched

--- app.js
+++ app.js
@@ -20,4 +20,4 @@
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
-  console.log(app);
+ // console.log(app);
--- login.js
+++ login.js
@@ -1,5 +1,6 @@
 import { 
-    getAuth, createUserWithEmailAndPassword,
+    getAuth, createUserWithEmailAndPassword, 
+    onAuthStateChanged, signInWithEmailAndPassword, signOut
 } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js"
 
 const auth = getAuth();
@@ -7,18 +8,62 @@ const auth = getAuth();
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const signUpBtn = document.getElementById("signup-btn");
+const signInBtn = document.getElementById("login-btn");
+const signOutBtn = document.getElementById("logout-btn");
+
+
+
+onAuthStateChanged(auth, (user) => {
+    //console.log(user);
+    if(user){
+        window.location.href = "second.html";
+    }else{
+        window.location.href = "login.html";
+    }
+
+});
 
 const signUpButtonPressed = async (e) => {
-    console.log("Pressed!");
+    //console.log("Pressed!");
     e.preventDefault();
 
     try{
 
         const userCredential  = await createUserWithEmailAndPassword(auth, email.value, password.value);
-        console.log(userCredential);
+       // console.log(userCredential);
+       
     }catch(error){ 
-        console.log(error.code);
+       console.log(error.code);
+       
     }
 };
 
+const signOutButtonPressed = async () => {
+    //console.log("Pressed!");
+    try{
+        await signOut(auth);
+        email.value = "";
+        password.value = "";
+        window.location.href = "login.html";
+
+    }catch(error){
+        console.log(error);
+    }
+
+};
+
+const signInButtonPressed = async (e) => {
+    //console.log("Pressed!");
+    e.preventDefault();
+    try{
+        await signInWithEmailAndPassword(auth, email.value, password.value);
+
+    }catch(error){
+        console.log(error);
+    }
+
+};
+
 signUpBtn.addEventListener("click", signUpButtonPressed);
+signInBtn.addEventListener("click", signInButtonPressed);
+signOutBtn.addEventListener("click", signOutButtonPressed);
